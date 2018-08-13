019452542a46a794daefcf754e0a1cf78c43ccc1
Tao Cheng <taocheng@qnap.com>
2018-08-13 11:06:43 +0800

    [skip ci] Webpack lib name with version.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path')
 
+const { version } = require('./package.json')
+
+const filename = `cq-websocket-${version}.min.js`
+
 module.exports = {
   mode: 'production',
   entry: path.join(__dirname, 'browser.js'),
   output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'cq-websocket.min.js'
+    path: path.join(__dirname, 'dist'),
+    filename
   }
 }
