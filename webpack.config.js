ed46949a7e448ace703eed5e73ca220ab7d25f20
MomoCow <momocow.me@gmail.com>
2019-02-24 05:44:19 +0000

    chore(browser): Add kaomojified bundle for fun. The browser bundle is no longer based on browser.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,37 @@
 const path = require('path')
+const KaomojifyWebpackPlugin = require('kaomojify-webpack-plugin')
 
-module.exports = {
+const COMMON_CONFIG = {
   mode: 'production',
-  entry: path.join(__dirname, 'browser.js'),
+  entry: path.join(__dirname, 'src', 'index.js'),
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'cq-websocket.min.js'
+    library: {
+      root: 'CQWebSocketSDK',
+      amd: 'cq-websocket',
+      commonjs: 'cq-websocket'
+    },
+    libraryTarget: 'umd',
+    libraryExport: '',
+    path: path.join(__dirname, 'dist')
   }
 }
+
+module.exports = [
+  { // minified bundle
+    ...COMMON_CONFIG,
+    output: {
+      ...COMMON_CONFIG.output,
+      filename: 'cq-websocket.min.js'
+    }
+  },
+  { // kaomojified bundle (x100 in size) (*´∇｀*)/
+    ...COMMON_CONFIG,
+    output: {
+      ...COMMON_CONFIG.output,
+      filename: 'cq-websocket.kaomojified.js'
+    },
+    plugins: [
+      new KaomojifyWebpackPlugin()
+    ]
+  }
+]
